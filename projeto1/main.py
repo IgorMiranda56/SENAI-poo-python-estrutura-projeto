305f501035a2fb5c02f5f7eb05e7fc322699a4c1
IgorMiranda56 <159519511+IgorMiranda56@users.noreply.github.com>
2024-09-09 14:21:20 +0000

    Projeto

---
+++ projeto1/main.py
@@ -0,0 +1,11 @@
+import os
+from models.pessoa import Pessoa
+from models.enums.sexo import Sexo
+from models.endereco import Endereco
+
+os.system("cls || clear")
+
+#Instanciando classe
+pessoa1 = Pessoa("Marta", 22, Sexo.FEMININO, Endereco("Rua A", 35))
+
+print(pessoa1)
---
+++ projeto1/models/endereco.py
@@ -0,0 +1,7 @@
+class Endereco:
+    def __init__(self, logradouro: str, numero: int) -> None:
+        self.logradouro = logradouro
+        self.numero = numero
+    
+    def __str__(self) -> str:
+        return (f"\nLogradouro: {self.logradouro} \nNúmero: {self.numero}")
